f0d4fd150514030845817d00d1f1433507137cb6
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-05-18 13:09:03 +0530

    Added Register User to the database(MongoDB)

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../model/userModel');
 
 // @desc Register user
 // @route /api/users
@@ -9,6 +12,31 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please Include all fields');
   }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error('User already exists');
+  }
+
+  // Hashing the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User 
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  }
+
   res.send('Register Route');
 });
 
