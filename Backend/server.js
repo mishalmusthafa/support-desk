6d66f94c98172728234749465d4924e655aa9cd3
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-05-31 00:43:15 +0530

    Prepare for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -11,15 +12,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to the Support Desk'
-  });
-});
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static 
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Welcome to the Support Desk API'
+    });
+  });
+}
 
 app.use(errorHandler);
 
